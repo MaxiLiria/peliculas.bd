6ed55dffcef4821ade7b0f2e86824ca23ab08e93
Maxi <maaxiliria@gmail.com>
2023-06-23 10:48:25 +0200

    second commit

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -8,12 +8,18 @@ import { ServicesService } from '../services/services.service';
 })
 export class FilmsComponent implements OnInit {
   movies!: any[]
+  filtroList: any[] = [];
 
   constructor (private api: ServicesService) {}
-
+   filtrar(filtro:any){
+    this.filtroList=this.movies.filter((movie)=> movie.title.toLowerCase().includes(filtro))
+   console.log(filtro);
+   
+   }
   ngOnInit(): void {
     this.api.getMovies().subscribe((data: any) => {
       this.movies = [...data];
+      this.filtroList = [...data];
     })
   }
 }
